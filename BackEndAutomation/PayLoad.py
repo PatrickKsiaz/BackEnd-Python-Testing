fd7fb132f0a7b1904465f560d003044d28ccfebf
Patryk Ksiazek <globalghost1@protonmail.com>
2025-07-01 19:24:52 +0200

    Add/DeleteFlowofAPI

---
+++ BackEndAutomation/PayLoad.py
@@ -0,0 +1,8 @@
+def addBookPayload():
+    body = {
+        "name": "Learn Appium Automation with Java",
+        "isbn": "bcd",
+        "aisle": "227",
+        "author": "John Foe" 
+    }
+    return body
--- BackEndAutomation/PostAPIexample.py
+++ BackEndAutomation/PostAPIexample.py
@@ -1,18 +1,27 @@
 import requests
+from PayLoad import *
 
+addBook_response = requests.post('http://216.10.245.166/Library/Addbook.php',
+              json=addBookPayload(),             
+headers={"Content-Type": "application/json"},)
 
-requests.post('http://216.10.245.166/Library/Addbook.php',
-              json={
-    "name": "Learn Appium with Java",
-    "isbn": "bcd",
-    "aisle": "227",
-    "author": "John Foe"
-},headers={"Content-Type": "application/json"},)
-
-print(addbook_response.json())
-response_json = addbook_response.json()
+print(addBook_response.json())
+response_json = addBook_response.json()
 print(type(response_json))
 
 bookId = response_json['ID']
 
-#Delete Book 
+#Delete Book  - 
+
+response_deleteBook = requests.post('http://216.10.245.166/Library/Deletebook.php',
+              json={
+"ID": bookId
+              },headers={"Content-Type": "application/json"},
+    )
+
+assert response_deleteBook.status_code == 200
+res_json = response_deleteBook.json()
+
+print(res_json['msg'])
+assert res_json['msg'] == 'book is successfully deleted'
+
