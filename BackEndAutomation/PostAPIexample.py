844bc8915eae9099a61f95322f223f15bb38154e
Patryk Ksiazek <globalghost1@protonmail.com>
2025-07-03 18:53:53 +0200

    changes2

--- BackEndAutomation/PostAPIexample.py
+++ BackEndAutomation/PostAPIexample.py
@@ -1,13 +1,19 @@
-import requests
+
 from PayLoad import *
+import configparser
+import json
+from utilities.configurations import *
+from utilities.resources import *
 
-addBook_response = requests.post('http://216.10.245.166/Library/Addbook.php',
-              json=addBookPayload(),             
-headers={"Content-Type": "application/json"},)
 
+import requests
+
+url = getConfig['API']['endpoint']+ ApiResources.addBook
+addBook_response = requests.post(url,json=addBookPayload("fefrewe"),headers=headers,)
 print(addBook_response.json())
 response_json = addBook_response.json()
 print(type(response_json))
+
 
 bookId = response_json['ID']
 
@@ -24,4 +30,6 @@ res_json = response_deleteBook.json()
 
 print(res_json['msg'])
 assert res_json['msg'] == 'book is successfully deleted'
+
+
 
