85f48e00eb6494cc5894ab84b20c7940b1784f51
Patryk Ksiazek <globalghost1@protonmail.com>
2025-07-14 13:06:04 +0200

    Connection Utility and pass SQL

--- BackEndAutomation/dbDemo.py
+++ BackEndAutomation/dbDemo.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 #host, database,username,passsword
 
-conn = mysql.connector.connect(host='localhost',database='PythonAutomation', user='root', password='rahulshettyacademy')
+
 
 print(conn.is_connected())
 
--- BackEndAutomation/utilities/configurations.py
+++ BackEndAutomation/utilities/configurations.py
@@ -1,10 +1,34 @@
 import configparser
+import mysql.connector
+from mysql.connector import Error
+
+
+
 
 def getConfig():
     config = configparser.ConfigParser()
     config.read('utilities/properties.ini')
     return config
 
+connect_config  = {
+    'user' : getConfig()['SQL']['user'],
+    'password' : getConfig()['SQL']['password'],
+    'host' : getConfig()['SQL']['host'],
+    'database' : getConfig()['SQL']['database'],
+}
+
+ 
 def getPassword():
     return "ddsfere"
+
+def getConncetion():
+    try:
+        conn = mysql.connector.connect(**connect_config)
+        if conn.is_connected():
+            print("Connected to MySQL database")
+            return conn
+    except Error as e:
+        print(e)
+
+
 
