072a8b72623435cdb32d57b554606efb45c676c2
Patryk Ksiazek <globalghost1@protonmail.com>
2025-07-14 10:02:14 +0200

    Changes 14/07

--- BackEndAutomation/Miscellanous.py
+++ BackEndAutomation/Miscellanous.py
@@ -17,8 +17,9 @@ se.cookies.update({'visit-month': 'February',})
 res = requests.get("https://httpbin.org/cookies", cookies={'visit-month': '2022',})
 print(res.text)
 
+
 #Attachments
-url = "https://petstore.swagger.io/pet/9843217/uploadImage"
+url = "https://petstore.swagger.io/v2/pet/9843217/uploadImage"
 files = {'file': open('C:\\Users\\Owner\\Documents\\ra.png', 'rb')}
 r = requests.post(url,files = files)
 print(r.status_code)
